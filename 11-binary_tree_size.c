b1dda12abef2618383a8c627a4504b81356f98e6
Dev-wonderful <adeonederful20@gmail.com>
2023-05-30 17:50:18 +0000

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,7 +7,7 @@
  *
  * Return: returns a value representing height
  */
-size_t check(binary_tree_t *node)
+size_t check(const binary_tree_t *node)
 {
 	size_t size = 1;
 	size_t left, right;
@@ -20,36 +20,24 @@ size_t check(binary_tree_t *node)
 		right = check(node->right);
 	else
 		right = 0;
-	size = left + right;
+	size += left + right;
 
 	return (size);
 }
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to the root node of the tree to be measured
  *
  * Return: height of tree or 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t height = 0;
-	size_t left, right;
+	size_t size;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		left = check(tree->left);
-	else
-		left = 0;
-	if (tree->right != NULL)
-		right = check(tree->right);
-	else
-		right = 0;
 
-	if (left >= right)
-		height += left;
-	else
-		height += right;
+	size = check(tree);
 
-	return (height);
+	return (size);
 }
