8daa57696dd9f50f66b4c5612829a719ad0fed56
Dev-wonderful <adeonederful20@gmail.com>
2023-05-30 15:17:08 +0000

    traverse a tree using in-order method

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - traverse the tree using inorder method
+ * @tree: pointer to the tree
+ * @func: pointer to a print function
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
